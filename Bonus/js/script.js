dded58a81205f7c7a65e8397af703bc85482b8c4
Fabrizio-padula-hub <fabriziopadula80@gmail.com>
2024-03-26 18:27:57 +0100

    feat: added img

---
+++ Bonus/js/script.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello word, welcome Vue',
+            imageSrc: './img/imgprova.jpeg'
+        };
+    }
+}).mount('#app');
